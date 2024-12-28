a7025892991406050030a5b73f2108026a5d7fca
Sensini24 <lit.universal_2l@hotmail.com>
2024-12-27 21:10:21 -0500

    Empezando migraciones y persistencia 2

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using DemoMinimalAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoMinimalAPI.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        // Lo que se pone en DbSet es lo que se va a mapear a la base de datos
+        public DbSet<Persona> Personas { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
+using DemoMinimalAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
